63663aa43a05e11e0c08aef601063aeb0841592b
김현지 <guswlhjk@naver.com>
2021-08-24 11:46:50 +0900

    페이지별 작업

--- app/src/main/java/com/nepplus/viewpager/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/nepplus/viewpager/adapters/MainViewPagerAdapter.kt
@@ -9,6 +9,17 @@ import com.nepplus.viewpager.fragements.NameFragment
 
 class MainViewPagerAdapter( fm : FragmentManager ) : FragmentPagerAdapter(fm) {
 
+    override fun getPageTitle(position: Int): CharSequence? {
+        if (position == 0 ) {
+            return "인사"
+
+        }
+        else if( position == 1) {
+            return "이름"
+        }
+        else { return "출생년도"}
+    }
+
     override fun getCount(): Int {
         //뷰페이저가 총 몇장짜리인지? return
 
